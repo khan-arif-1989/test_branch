4cc05dcc2959eca70f18fe761be9951c88068173
Arifali.Khan <P@ssw0rd&5>
2019-01-21 21:44:56 +0400

    testing for jenkins build

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -1,6 +1,6 @@
 public class MainApplication {
     public static void main(String[] args) {
-        System.out.println("Testing for Git Application");
+        System.out.println("Testing for Git Application for test");
         System.out.println("This is amazing");
     }
 }
